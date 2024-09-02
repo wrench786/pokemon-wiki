16f4ac92c38c3863f9c68101dce5a56075971b25
wrench786 <wrench786@gmail.com>
2024-09-02 18:48:17 +0600

    footer added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBannerComponent } from './component/top-banner/top-banner.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopBannerComponent
+    TopBannerComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,

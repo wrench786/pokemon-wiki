0132ea36866f2ccbb0011911531c0033f8fb1db3
wrench786 <wrench786@gmail.com>
2024-09-02 18:41:57 +0600

    top banner component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBannerComponent } from './component/top-banner/top-banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBannerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
-
-
-
   ],
   providers: [],
   bootstrap: [AppComponent]
